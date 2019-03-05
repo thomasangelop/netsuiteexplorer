aebed58c685c149061946978adcd5a783d295e1c
Thomas Pinataro <tpinataro@vitalimages.com>
2019-03-05 11:50:07 -0600

    created basic file structure and added text to main page

---
+++ src/Header/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <h1>NetSuite Explorer</h1>
+        <h4>Find the useful help tips without digging</h4>
+        <h4>through NetSuite Help Documentation.</h4>
+      </div>
+    );
+  }
+}
+
+export default Header;
